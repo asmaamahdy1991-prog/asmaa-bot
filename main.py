9229e9a0ba4d72ab491ca776f92f39701b92d8aa
asmaamahdy1991-prog <asmaamahdy1991@gmail.com>
2026-04-30 19:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ async def continue_list(message: Message):
     await send_new_bottom(message.chat.id)
 
 
-@dp.message(F.text == "بدء قايمة جديده")
+@dp.message(F.text.in_({"بدء قايمة جديده", "بدء قائمة جديده"}))
 async def new_list_text(message: Message):
     if not await is_group_admin(message.chat.id, message.from_user.id):
         return
